31503bd1789efdde98bc83f5f0d9d523d64a9d17
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-11-21 12:35:03 -0600

    Add files via upload

---
+++ FileStatistics.java
@@ -0,0 +1,15 @@
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.io.IOException;
+
+public class FileStatistics {
+
+    public static void main(String[] args) throws IOException {
+        Path file = Paths.get("/root/sandbox/FileStatistics.java");
+        BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+        System.out.println("File name is " + file.getFileName());
+        System.out.println("Folder name is " + file.getParent().getFileName());
+        System.out.println("File's size is " + attr.size());
+        System.out.println("File's creation time is " + attr.creationTime());
+    }
+}
